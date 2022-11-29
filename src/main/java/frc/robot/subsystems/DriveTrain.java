bbe1ad1343471f799e18838c9a169816fe34ca8f
shadowdragon2805 <lexue28@gmail.com>
2022-11-28 22:18:28 -0500

    silly wackus

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -26,69 +26,67 @@ import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
   /** Creates a new DriveTrain. */
-  
-  private Encoder m_leftEncoder = new Encoder(0, 1);
+  // These represent our regular encoder objects, which we would
+  // create to use on a real robot.
+  private Encoder m_leftEncoder = new Encoder(0, 1); //4 unique #s to act as ports
   private Encoder m_rightEncoder = new Encoder(2, 3);
 
-  private EncoderSim m_leftEncoderSim = new EncoderSim(m_leftEncoder);
-  private EncoderSim m_rightEncoderSim = new EncoderSim(m_rightEncoder);
-
+  // Create our gyro object like we would on a real robot.
   private AnalogGyro m_gyro = new AnalogGyro(1);
 
-  private AnalogGyroSim m_gyroSim = new AnalogGyroSim(m_gyro);
+  //make a differential drive
+  /*
+  This constructor takes the following parameters:
 
-  private DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(
-    LinearSystemId.identifyDrivetrainSystem(Constants.KvLinear, Constants.KvLinear, Constants.KvAngular, Constants.KaAngular),
-    DCMotor.getNEO(2),
-    7.29,
-    0.7112,
-    Units.inchesToMeters(3),
+The type and number of motors on one side of the drivetrain.
 
-    VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
-     
-  private MotorControllerGroup m_leftMotors = new MotorControllerGroup(new PWMSparkMax(0), new PWMSparkMax(1));
-  private MotorControllerGroup m_rightMotors = new MotorControllerGroup(new PWMSparkMax(2), new PWMSparkMax(3));
+The gear ratio between the motors and the wheels as output torque over input torque (this number is usually greater than 1 for drivetrains).
 
-  private DifferentialDrive m_differentialDrive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+The moment of inertia of the drivetrain (this can be obtained from a CAD model of your drivetrain. Usually, this is between 3 and 8 ).
 
-  private Field2d m_field = new Field2d();
+The mass of the drivetrain (it is recommended to use the mass of the entire robot itself, as it will more accurately model the acceleration characteristics of your robot for trajectory tracking).
 
-  private DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0));
+The radius of the drive wheels.
 
+The track width (distance between left and right wheels).
+
+Standard deviations of measurement noise: this represents how much measurement noise you expect from your real sensors. The measurement noise is an array with 7 elements, with each element representing the standard deviation of measurement noise in x, y, heading, left velocity, right velocity, left position, and right position respectively. This option can be omitted in C++ or set to null in Java if measurement noise is not desirable.
+  */
+  // Create the simulation model of our drivetrain.
+DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(
+  DCMotor.getNEO(2),       // 2 NEO motors on each side of the drivetrain.
+  7.29,                    // 7.29:1 gearing reduction.
+  7.5,                     // MOI of 7.5 kg m^2 (from CAD model).
+  60.0,                    // The mass of the robot is 60 kg.
+  Units.inchesToMeters(3), // The robot uses 3" radius wheels.
+  0.7112,                  // The track width is 0.7112 meters.
+
+  // The standard deviations for measurement noise:
+  // x and y:          0.001 m
+  // heading:          0.001 rad
+  // l and r velocity: 0.1   m/s
+  // l and r position: 0.005 m
+  VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
+
+  
+  //creates a new DriveTrain, initializer
   public DriveTrain() {
-    m_leftEncoder.setDistancePerPulse(Constants.ENCODER_DISTANCE_PER_PULSE);
-    m_rightEncoder.setDistancePerPulse(Constants.ENCODER_DISTANCE_PER_PULSE);
 
-    m_leftEncoder.reset();
-    m_rightEncoder.reset();
-
-    m_differentialDrive.setSafetyEnabled(false);
-    
-    SmartDashboard.putData("Field", m_field);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    m_odometry.update(m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
-    m_field.setRobotPose(m_odometry.getPoseMeters());
+
   }
 
   @Override
   public void simulationPeriodic(){
-    m_driveSim.setInputs(m_leftMotors.get()* RobotController.getInputVoltage(), m_rightMotors.get()* RobotController.getInputVoltage());
-
-    m_driveSim.update(0.02);
-
-    m_leftEncoderSim.setDistance(m_driveSim.getLeftPositionMeters());
-    m_leftEncoderSim.setRate(m_driveSim.getLeftVelocityMetersPerSecond());
-    m_rightEncoderSim.setDistance(m_driveSim.getRightPositionMeters());
-    m_rightEncoderSim.setRate(m_driveSim.getRightVelocityMetersPerSecond());
-    m_gyroSim.setAngle(-m_driveSim.getHeading().getDegrees());
+    //  This method will act like periodic but only run in simulations
+    
   }
 
-  public void drive(double throttle, double rotate, boolean squaredInput) {
-    m_differentialDrive.arcadeDrive(throttle, -rotate, squaredInput);
+  public void drive(double asDouble, double asDouble2, boolean b) {
   }
 
 }
